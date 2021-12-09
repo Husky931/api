6c2f9c2aa91e894bc40d6b75070c25c9f4a80620
Xz33M Hask <pecevgligor@protonmail.com>
2021-12-09 09:49:38 +0100

    transfer routes to separate directory

--- server.js
+++ server.js
@@ -1,25 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const PORT = process.env.PORT || 5000
+const bootcamps = require('./routes/bootcamps.js')
 
 dotenv.config({path: "./config/config.env"})
-const app = express()
-const PORT = process.env.PORT || 5000
 
-app.get('/api/v1/bootcamps',(req, res) => {
-  res.status(200).json({success: true, msg: 'Show all bootcamps'})
-})
-app.get('/api/v1/bootcamps/:id',(req, res) => {
-  res.status(200).json({success: true, msg: `Get bootcamp ${req.params.id}`})
-})
-app.post('/api/v1/bootcamps',(req, res) => {
-  res.status(200).json({success: true, msg: 'Create new bootcamp'})
-})
-app.put('/api/v1/bootcamps/:id',(req, res) => {
-  res.status(200).json({success: true, msg: `Update bootcamp ${req.params.id}`})
-})
-app.delete('/api/v1/bootcamps/:id',(req, res) => {
-  res.status(200).json({success: true, msg: `Delete bootcamp ${req.params.id}`})
-})
+const app = express()
+
+app.use('/api/v1/bootcamps', bootcamps)
 
 
 app.listen(PORT, console.log(`server is running in ${process.env.NODE_ENV} mode in ${PORT}`))
